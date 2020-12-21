c02efab902d02893f2909ecfb9f936878e61abc8
Roy Sparrow <rvxl2013@gmail.com>
2020-12-21 18:28:20 +0800

    add: Array extensions

---
+++ SparrowToolbox/SparrowToolbox/Extension/ArrayExtensions.swift
@@ -0,0 +1,19 @@
+//
+//  ArrayExtensions.swift
+//  SparrowToolbox
+//
+//  Created by Roy Sparrow on 2020/12/21.
+//  Copyright © 2020 SparrowStudio. All rights reserved.
+//
+
+import Foundation
+
+extension Array {
+    
+    // use case:
+    //
+    // guard let element = elementArray[safe: 0] else { return }
+    subscript (safe index: Index) -> Element? {
+        return index >= 0 && index < count ? self[index] : nil
+    }
+}
