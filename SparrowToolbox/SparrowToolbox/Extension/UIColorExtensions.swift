e3f13507c089420ce74c4e52c6f26eacfc0fdb4d
Roy Sparrow <rvxl2013@gmail.com>
2021-01-06 18:35:17 +0800

    add: UIColor extensions

---
+++ SparrowToolbox/SparrowToolbox/Extension/UIColorExtensions.swift
@@ -0,0 +1,16 @@
+//
+//  UIColorExtensions.swift
+//  SparrowToolbox
+//
+//  Created by Roy Sparrow on 2021/1/6.
+//  Copyright © 2021 SparrowStudio. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    convenience init(red: Int, green: Int, blue: Int, alpha: CGFloat = 1) {
+        self.init(red: CGFloat(red) / 255, green: CGFloat(green) / 255, blue: CGFloat(blue) / 255, alpha: alpha)
+    }
+}
